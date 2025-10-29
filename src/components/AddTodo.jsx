ad94f081fe4e3432a58f573fa4f6ab3173bb01cb
Rajveer-Singh-Git <rajveersinghbareth8@gmail.com>
2025-10-29 20:38:02 +0530

    configured main.jsx & app.jsx and finalized functionality

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -18,7 +18,7 @@ function AddTodo() {
       <input
         type="text"
         className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-        placeholder="What Would you like to do "
+        placeholder="Enter your task"
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
